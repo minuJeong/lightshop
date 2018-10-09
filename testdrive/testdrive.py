67247d9153b87785e25b9e9d4a0248de53b593cd
Minu Jeong <minu.hanwool@gmail.com>
2018-10-09 09:20:51 +0800

    update testdrive

--- testdrive/testdrive.py
+++ testdrive/testdrive.py
@@ -13,34 +13,34 @@ def source(args):
         content = content.replace(f"%%{key}%%", str(value))
     return content
 
-X = 32
-Y = 32
-WIDTH = 512
-HEIGHT = 512
-CHANNEL = 4
+# W = X * Y  // for each run, handles one line of pixels
+# execute compute shader for H times to complete
+X = 8
+Y = 8
+W = 64
+H = 64
 definer = {
     "X": X,
     "Y": Y,
-    "WIDTH": WIDTH,
-    "HEIGHT": HEIGHT,
-    "CHANNEL": CHANNEL,
+    "WIDTH": W,
+    "HEIGHT": H,
 }
 
 context = moderngl.create_standalone_context(require=430)
-buffer_a = context.buffer(np.random.uniform(0.0, 100.0, (WIDTH, HEIGHT, CHANNEL)).astype('f4'))
-buffer_b = context.buffer(np.zeros((WIDTH, HEIGHT, CHANNEL)).astype('f4'))
 compute_shader = context.compute_shader(source(definer))
 
+data = np.random.uniform(0.0, 100.0, (W, H))
+buffer_a = context.buffer(data.astype('f4'))
+buffer_b = context.buffer(np.ones((W, H)).astype('f4'))
 buffer_a.bind_to_storage_buffer(0)
-buffer_b.bind_to_storage_buffer(1)
+buffer_b.bind_to_storage_buffer(2)
 
-texres = WIDTH * HEIGHT
-groupres = X * Y
-for x in range(0, math.ceil(texres / groupres)):
-    compute_shader.run(group_x=x, group_y=1)
+for y in range(0, H):
+    compute_shader.run(group_x=1, group_y=y)
 
 # print out
 output = np.frombuffer(buffer_b.read(), dtype=np.float32)
-output = output.reshape((WIDTH, HEIGHT, CHANNEL))
-for row in output:
-    print(',\t'.join(map(lambda e: f"{e:.3f}", [x[0] for x in row.tolist()])))
+output = output.reshape((W, H))
+
+np.set_printoptions(linewidth=100000, threshold=np.NaN)
+print(output)
