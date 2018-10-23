4a9eac35cb194a520b240cabf388f099a9a30ab3
minu jeong <minu.hanwool@gmail.com>
2018-10-23 18:23:28 +0800

    update testdrive

--- testdrive.py
+++ testdrive.py
@@ -8,14 +8,8 @@ import moderngl as mg
 from PyQt5 import QtWidgets
 
 
-class Ball(object):
-    radius = 0.02
-    x = 0
-    y = 0
-    vx = 0
-    vy = 0
-
 class Renderer(QtWidgets.QOpenGLWidget):
+    COUNT = 1024
 
     def __init__(self):
         super(Renderer, self).__init__()
@@ -23,20 +17,6 @@ class Renderer(QtWidgets.QOpenGLWidget):
         self.setMinimumSize(W, H)
         self.setMaximumSize(W, H)
         self.viewport = (0, 0, W, H)
-
-        COUNT = 500
-        self.scene_items = []
-        for i in range(COUNT):
-            item = Ball()
-            angle = (i / COUNT) * math.pi * 2.0
-            dist = 0.125
-            item.radius = random.random() * 0.04 + 0.01
-            item.x = math.cos(angle) * dist
-            item.y = math.sin(angle) * dist
-            v = random.random() * 0.45 + 0.15
-            item.vx = math.cos(angle) * v
-            item.vy = math.sin(angle) * v
-            self.scene_items.append(item)
 
     def initializeGL(self):
         self.context = mg.create_context()
@@ -75,59 +55,101 @@ class Renderer(QtWidgets.QOpenGLWidget):
             1, 2, 3,
         ]).astype('i4')
         self.index_buffer = self.context.buffer(index_data.tobytes())
-        self.prevframe = time.time()
 
-    def paintGL(self):
-        self.context.viewport = self.viewport
-
-        vbo_data = np.array([
-            # pos xy      u    v
+        self.vbo_data = np.array([
+            # x     y     u    v
             [-1.0, -1.0,  0.0, 0.0, ],
             [-1.0, +1.0,  0.0, 1.0, ],
             [+1.0, -1.0,  1.0, 0.0, ],
             [+1.0, +1.0,  1.0, 1.0, ],
         ]).astype('f4')
-        self.vaos = []
-        for item in self.scene_items:
-            item_vertex = vbo_data.copy()
 
-            # apply attr
-            item_vertex[:, [0, 1]] *= item.radius * 2.0
-            item_vertex[:, [0, 1]] += [item.x, item.y]
+        self.prevframe = time.time()
+
+        # calc position with compute shader
+        self.compute_shader = self.context.compute_shader("""
+        #version 430
+        layout(local_size_x=1024, local_size_y=1, local_size_z=1) in;
+        layout(binding=0) buffer balls
+        {
+            float rad[1];
+            vec2 pos[1];
+            vec2 vel[1];
+        };
+
+        void main()
+        {
+            const int x = int(gl_LocalInvocationID.x);
+            const float r = rad[x];
+
+            vec2 p = pos[x];
+            vec2 v = vel[x];
+            p += v;
+            if (p.x - r <= -1.0)
+            {
+                p.x = -1.0 + r;
+                v.x *= -1.0;
+            }
+            else if (p.x + r >= 1.0)
+            {
+                p.x = 1.0 - r;
+                v.x *= -1.0;
+            }
+
+            if (p.y - r <= -1.0)
+            {
+                p.y = -1.0 + r;
+                v.y *= -1.0;
+            }
+            else if (p.y + r >= 1.0)
+            {
+                p.y = 1.0 - r;
+                v.y *= -1.0;
+            }
+
+            pos[x] = p;
+            vel[x] = v;
+        }
+        """)
+
+        compute_data = []
+        for i in range(Renderer.COUNT):
+            _angle = (i / Renderer.COUNT) * math.pi * 2.0
+            _dist = 0.125
+            radius = random.random() * 0.04 + 0.04
+            x = math.cos(_angle) * _dist
+            y = math.sin(_angle) * _dist
+            _v = random.random() * 0.000045 + 0.000025
+            vx = math.cos(_angle) * _v
+            vy = math.sin(_angle) * _v
+            compute_data.append([radius, x, y, vx, vy])
+
+        self.compute_buffer = self.context.buffer(
+            np.array(compute_data).astype('f4').tobytes()
+        )
+        self.compute_buffer.bind_to_storage_buffer(0)
+
+    def paintGL(self):
+        self.context.viewport = self.viewport
+
+        data = np.frombuffer(self.compute_buffer.read(), dtype=np.float32)
+        data = data.reshape((Renderer.COUNT, 5))
+        self.vaos = []
+        for i, item in enumerate(data):
+            item_vertex = self.vbo_data.copy()
+            item_vertex[:, [0, 1]] *= item[0]
+            item_vertex[:, 0] += item[1]
+            item_vertex[:, 1] += item[2]
             item_vertex_buffer = self.context.buffer(item_vertex.tobytes())
-            self.buffer_object = [
+            bo = [
                 (item_vertex_buffer, '2f 2f', 'in_vert', 'in_uv'),
             ]
+            self.context.vertex_array(
+                self.program, bo, self.index_buffer
+            ).render()
 
-            vao = self.context.vertex_array(self.program, self.buffer_object, self.index_buffer)
-            self.vaos.append(vao)
-
-        for vao in self.vaos:
-            vao.render()
-
+        self.compute_shader.run(group_x=Renderer.COUNT)
         self.update()
-
-        newtime = time.time()
-        dt = newtime - self.prevframe
-        self.prevframe = newtime
-
-        for item in self.scene_items:
-            item.x += item.vx * dt
-            item.y += item.vy * dt
-
-            if item.x - item.radius <= -1.0:
-                item.x = -1.0 + item.radius
-                item.vx *= -1
-            elif item.x + item.radius >= 1.0:
-                item.x = 1.0 - item.radius
-                item.vx *= -1
-
-            if item.y - item.radius <= -1.0:
-                item.y = -1.0 + item.radius
-                item.vy *= -1
-            elif item.y + item.radius >= 1.0:
-                item.y = 1.0 - item.radius
-                item.vy *= -1
 
 
 def main():
