56e58ca513a29aef5af9ba1919614ee8319677c3
Minu Jeong <minu.hanwool@gmail.com>
2018-12-02 14:50:58 +0900

    add _common.py

---
+++ _common.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+
+def _read(path, args):
+    with open(path, 'r') as fp:
+        content = fp.read()
+
+    for k, v in args.items():
+        content = content.replace(f"%{k}", str(v))
+
+    return content
+
+def _flatten_array(data):
+    data = np.multiply(data, 255.0)
+    data = data.astype(np.uint8)
+    return data
